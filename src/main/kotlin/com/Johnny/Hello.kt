a84e8680fa39693bb7d2e9a4a5875c3f2f545950
Johnny <tsunglin.lin@gmail.com>
2019-08-19 16:42:33 +0800

    first commit

---
+++ src/main/kotlin/com/Johnny/Hello.kt
@@ -0,0 +1,15 @@
+package com.Johnny
+
+fun main() {
+    //println("Hello Kotlin")
+    //Human().hello()
+    var h = Human() ;
+    h.hello()
+}
+
+class Human() {
+
+    fun hello() {
+        println(" Hello Kotlin")
+    }
+}
