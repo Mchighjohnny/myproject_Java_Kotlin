37e92fc36b8f3714b01a58523a6e0111db070e04
Johnny <tsunglin.lin@gmail.com>
2019-08-19 16:54:40 +0800

    second commit

--- src/main/kotlin/com/Johnny/Hello.kt
+++ src/main/kotlin/com/Johnny/Hello.kt
@@ -5,6 +5,18 @@ fun main() {
     //Human().hello()
     var h = Human() ;
     h.hello()
+
+    // var --> value 可以被改變
+    // val --> valiable 不可以被改變
+
+    val age : Int = 19 ;
+    val age2 = 19 ; //自動揣摩資料型態
+    // (x)   age2 = 20 --> val 資料型態被固定
+
+    var name : String ; // 事先定義資料型態
+    name = "Johnny"
+
+
 }
 
 class Human() {
